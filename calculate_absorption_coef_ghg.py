edfa92b8d7285313f2e10333fca7625515bf1839
seancraven <seanpietercraven@gmail.com>
2022-07-21 15:42:51 +0100

    first iteration of calculation

--- calculate_absorption_coef_ghg.py
+++ calculate_absorption_coef_ghg.py
@@ -8,12 +8,14 @@
     and building a database after that enables much faster 
     atmospheric model calculations after initial setup. 
 '''
-import hapi
+import sqlite3
+from sqlite3 import Error
 import isa
 import numpy as np
 import os, sys
+from optical_depth_utilities import optical_depth
+
 class HiddenPrints:
-    '''Stops module functions printing ovemuch'''
     def __enter__(self):
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
@@ -21,52 +23,171 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
-class GhgAbundance:
+with HiddenPrints():
+    import hapi
+class Ghg:
     '''
     records ghg abundance in ppb ghg abundance assumed constant. Up to implemented
-    cealing of 10km.  
+    cealing of 10km. 
     
     This is the same assumption as taken by MODTRAN which achieves accuracies
     better than 1k accuraciy on thermal brightness temperature.
     '''
-    CO2 = 411*1000
-    CH4 = 1893
-    N2O = 327
-    H20 = 25 * 10**6
-
+    ppm = {'CO2': 411*1000,
+        'CH4': 1893,
+        'N2O': 327,
+        'H2O': 25 * 10**6
+    }
+    ids = {'CO2': 2,
+        'CH4': 6,
+        'N2O':4,
+        'H2O':1
+    }
+        
 def ghg_lbl_download():
-    ''' Downloads and Stores Line by line Data for 5 most abundent ghg.
+    ''' Downloads and Stores Line by line Data for 4 most abundent ghg.
         if further gases are required add name and HITRAN id to 
         molecule_id_dict. Data Is collected from HITRAN.
         
         assumes only most abundent isotopologue is required.
     '''
-    hapi.db_begin("./spectral_line_db.db")
+    hapi.db_begin("./spectral_line.db")
     isotopologue = 1 # only want main isotopologue
     min_wavenumber = 0
-    max_wavenumber = 5000 ## spectral flux density(watts m^(-2) m^-1) is negliable beyond this region
-    molecule_id_dict = {'H2O': 1, 'CO2': 2, 'CH4': 6, 'N2O': 4}
-    for gas, id in molecule_id_dict.items():
+    max_wavenumber = 4000 ## spectral flux density(watts m^(-2) m^-1) is negliable beyond this region
+    for gas, id in Ghg.ids.items():
         with HiddenPrints():
             hapi.fetch(gas,id, isotopologue, min_wavenumber, max_wavenumber)
-        
-def ghg_crossection_calculate():
-    '''
-    Calculate Crossections for main GHG
-    '''
-    trop_bound_alt = 10*1000
-    atltitudes = np.arange(0,trop_bound_alt, 100)
-    temp, pressure,  = isa.get_temperature(atltitudes), isa.get_pressure(atltitudes) 
-    density = isa.get_density(atltitudes)
-    ghg_gases = list(hapi.tableList())
-    if 'sampletab' in ghg_gases: # Deals with bug in hapi.tableList()
-        ghg_gases.remove('sampletab')
-    for T, P, Dens in zip(temp,pressure, density):
-        
-        
+
+  
+def ghg_od_calculate(gas,alt):
+
+    temp = isa.get_temperature(alt)
+    pressure = isa.get_pressure(alt)
+    press_0 = isa.get_pressure(0)
+    with HiddenPrints():
+        nu, coef = hapi.absorptionCoefficient_Voigt(SourceTables=gas,
+                                                Environment={"T":temp,
+                                                    "p":pressure/press_0
+                                                },
+                                                Diluent={"air":1.0}
+    )
+    with HiddenPrints():
+        od = optical_depth(alt-500, alt+500,Ghg.ppm[gas], coef)
+    return nu , od
         
 
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database. 
+        db_file is a path of the form:  
+        /path/to/file/name_of_database.db
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        return conn
+    except Error as e:
+        print(e)
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = """INSERT INTO gases(mol_id,mol_name,mol_ppm)
+            VALUES(?,?,?)"""
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+    return cur.lastrowid
+def pragma(connection):
+    connection.execute('PRAGMA journal_mode = OFF;')
+    connection.execute('PRAGMA synchronous = 0;')
+    connection.execute('PRAGMA cache_size = 1000000;')  # give it a GB
+    connection.execute('PRAGMA locking_mode = EXCLUSIVE;')
+    connection.execute('PRAGMA temp_store = MEMORY;')
+
+def main():
+    with HiddenPrints():
+        ghg_lbl_download()
+    create_table_gas_sql = """CREATE TABLE IF NOT EXISTS gases (
+                                mol_id integer PRIMARY KEY, 
+                                mol_name text NOT NULL, 
+                                mol_ppm real NOT NULL
+                                );"""
+    create_table_abs_coef_sql = """CREATE TABLE IF NOT EXISTS optical_depths (
+                                    mol_id INTEGER, 
+                                    altitude TEXT NOT NULL, 
+                                    wave_no  REAL NOT NULL,
+                                    abs_coef REAL NOT NULL,
+                                    PRIMARY KEY (mol_id, altitude, wave_no),
+                                    FOREIGN KEY (mol_id) REFERENCES gases (mol_id)
+                                );"""
+              
+    conn = create_connection(r'./test.db')
+    # Use sqlite3 PRAGMA for faster loading
+    pragma(conn)
+    if conn is not None:
+        create_table(conn, create_table_gas_sql)
+        create_table(conn, create_table_abs_coef_sql)
+        print('created tabels')
+    else:
+        print('Didnt Work')
+    # Populating Gases
+    for (mol_name, mol_id), (_,mol_ppm) in zip(Ghg.ids.items(), Ghg.ppm.items()):
+        mol_task = (mol_id,mol_name,mol_ppm)    
+        try:
+            create_task(conn, mol_task)
+        except sqlite3.IntegrityError:
+            cur = conn.cursor()
+            sql = "SELECT mol_name from gases WHERE mol_name = ?;"
+            cur.execute(sql,(mol_name,))
+            added_mols = cur.fetchall()
+            print(f"{added_mols} already in database")
+    altitudes = np.arange(500,30500,1000)
+    for gas, ids in Ghg.ids.items():
+        for alt in altitudes:
+            wav_number, od = ghg_od_calculate(gas, alt)
+            for nu, tau in zip(wav_number, od):
+                try:
+                    conn.execute("INSERT INTO optical_depths VALUES(?,?,?,?)",
+                        (ids, alt,nu,tau)
+                        )
+                except sqlite3.IntegrityError:
+                    sql = """SELECT * from optical_depths
+                        WHERE mol_id = ? and
+                        WHERE altitude = ? and
+                        WHERE wave_no = ?;
+                        """
+                    cur = conn.cursor()
+                    cur.execute(sql,(Ghg.ids[gas],alt, nu))
+                    query_result = cur.fetchall()
+                    if not query_result:
+                        print(f'''Failed to add \n
+                              molecule:{gas} \n
+                              altitude:{alt} \n
+                              wavenumber:{nu}
+                              ''')
+    conn.commit()
 if __name__ == "__main__":
-    ghg_lbl_download()    
-        
-    print(GhgAbundance.CO2)
+    main()
+    
---
+++ optical_depth_utilities.py
@@ -0,0 +1,49 @@
+'''
+    File to store basic functions for atmospheric calculations.
+'''
+import isa
+from scipy.integrate import quad
+from scipy import constants
+def number_density(alt):
+    '''
+    Returns Number of particles per m^3
+    assuming ideal gas.
+
+    Args:
+        alt (float): Altitude in meters.
+
+    Returns:
+        float: n(alt) in molecules/m^3
+    '''
+    mass_of_air = 28.9647 *10**-3/ constants.N_A  #conver kg/m^3 to molecule/m^3
+    return isa.get_density(alt)/mass_of_air
+def particle_per_sq_m(alt_0, alt_1):
+    '''
+    returns number of particles per square meter
+    between two altitudes
+
+    Args:
+        alt_0 (float): altitude in meters
+        alt_1 (float): altitude in meters
+
+    Returns:
+        float:  
+    '''
+    return quad(number_density, alt_0, alt_1)[0]
+
+def optical_depth(alt_0, alt_1, ppm_conc, abs_coef):
+    '''
+    Calculates optical depth m^-1, between two altitudes.
+    This quantity is often referred to symbolically as tau.
+    
+    Args:
+        alt_0 (float): altitude in meters
+        alt_1 (float): altitude in meters
+        ppm_conc (float): parts per million concentration of the gas
+        abs_coef (np.array): absorbrion coefficient array
+
+    Returns:
+        np.array: optical depth of gas between two altitudes
+    '''
+    particles = particle_per_sq_m(alt_0, alt_1)*ppm_conc * 10**(-9)
+    return particles*abs_coef*10**(-4) #10^-4 factor from cm^2->m^2 conv
